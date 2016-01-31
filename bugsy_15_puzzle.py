b6e6863f4264359072b8605076c9557421335a80
trattner <trattner@mit.edu>
2016-01-31 18:58:57 -0500

    testing commits fo realz

--- bugsy_15_puzzle.py
+++ bugsy_15_puzzle.py
@@ -1,4 +1,4 @@
-#for leslie k
+#for leslie kaelbling bling bling
 class Puzzle:
     def __init__(self, starting_state):
         self.goal_state=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0)
