f41e5dd876d534a9459fe03a8336a48034fdded2
trattner <trattner@mit.edu>
2016-01-31 18:46:06 -0500

    changed from 8 states to 15 puzzle

--- bugsy_15_puzzle.py
+++ bugsy_15_puzzle.py
@@ -1,23 +1,23 @@
 class Puzzle:
     def __init__(self, starting_state):
-        self.goal_state=(1, 2, 3, 4, 5, 6, 7, 8, 0)
+        self.goal_state=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0)
         self.initial_state=starting_state
         return None
 
     def next_states(self, s):
         i = s.index(0)
         validswaps=[]
-        v1 = i-3
-        v2 = i+3
+        v1 = i-4
+        v2 = i+4
         v3 = i - 1
         v4 = i + 1
         if v1>=0:
             validswaps.append(v1)
-        if v2<=8:
+        if v2<=15:
             validswaps.append(v2)
-        if v3%3<i%3:
+        if v3%4<i%4:
             validswaps.append(v3)
-        if v4%3>i%3:
+        if v4%4>i%4:
             validswaps.append(v4)
         out = []
         for v in validswaps:
