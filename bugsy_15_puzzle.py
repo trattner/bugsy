816cc5d8fbc0787d083614cbb0843d62bdcd21cf
Michael Amoako <mamoako@dhcp-18-111-71-134.dyn.mit.edu>
2016-02-02 10:35:40 -0500

    figured out u hat!

--- bugsy_15_puzzle.py
+++ bugsy_15_puzzle.py
@@ -83,24 +83,32 @@ class Puzzle:
 
 #make bugsy happen
 
-def bugsy(puzzle, u):
+def bugsy(puzzle, u, goal_state_d):
     '''
     for later: be complicated...A search function which takes input Utility, Problem Instance (with initial state, goal state, and successor function)
     for now: be simple: 15 puzzle instance (goal, initial, next_states), Utility func, g* calculated by graph distance bt states
     '''
-
+    start_time = time.time()
     closed = []
-    frontier = [(-5, puzzle.initial_state, [])]
+    frontier = [(-5, puzzle.initial_state, [], start_time)]
     #States are composed of (current state - tuple, parent state - tuple, cost - int)
     #State: tuple - (cost, current, parent)
     expansion_count = 0
-    start_time = time.time()
+    delay = 0
+    total_delay_time = 0
+    total_exp_time = 0
+    t_exp = 0
+
     while len(frontier) > 0:
         #finds utility of each node on every iteration
         current = heapq.heappop(frontier)
         if puzzle.goal_state == current[1]:
             return current[2]
         closed.append(current)
+        if not expansion_count == 0:
+            delay  += total_delay_time / expansion_count
+        #calc exp time
+        t_exp_1 = time.time()
         for next in puzzle.next_states(current[1]):
             parent_path = current[2][:]
             parent_path.append(current[1])
@@ -113,33 +121,80 @@ def bugsy(puzzle, u):
             else:
                 expansion_count += 1
                 heapq.heappush(frontier,child)
-        if expansion_count > 5:
-            if type(log(expansion_count, 2)) is int:
-                '''
-                for i in range(len(frontier)):
-                    frontier[i][0] = u(start_time, w_t, w_f, g, frontier[i][0])
-                '''
-                frontier = [(u(start_time, w_t, w_f, g, frontier[i][1]), frontier[i][1], frontier[i][2]) for i in range(len(frontier))]
-                #rereates the whole frontier with the tuples
-                heapq.heapify(frontier)
+        total_exp_time+=time.time()-t_exp_1
+        t_exp = total_exp_time/expansion_count
+        frontier = find_uhat(frontier, expansion_count)
     return 'while ended'
 
+def convert_to_tuples(state):
+    output = {}
+    for i in range(len(state)):
+        x = i % 4
+        w = int(i / 4)
+        output[start[i]] = (x, w)
+    return output
 
-
-def u(starting_time, w_t, w_f, g, node):
+def u(delay, t_exp, w_t, w_f, g, node):
     '''
     utility = something here ****
     Involving w_t and w_f
+
+    ^
+    U = max { max( n in frontier) - (w_f *  f(n) + w_t * d(n) * delay * t_exp)}
+                                            ^
+    You expand the node that produces the   U value above
+
+    d(n) = Manhattan distance
+    f(n) = g*(n) + d(n)
+
+    Calculate d:
+        -Loop and use the difference between the index of each number
+        -in current state and the index of those same numbers in goal state
+
+
     '''
-    t = time.time() - starting_time
-    utility = -1 * (w_f * g(node) + w_t * t)
+    '''
+
+
+
+    '''
+    utility = -1 * (w_f * g(node) + w_t * delay * t_exp)
     return utility
 
 def g(node):
     if type(node) is not None:
         return len(node[2])
 
+goal_state_dict = convert_to_tuples(puzzle.goal_state)
+
+def man_dist(puzzle_state, goal_state_dict):
+    dict_puzzle = convert_to_tuples(puzzle_state)
+    d = 0
+    for i in xrange(1, len(goal_state_dict) + 1):
+        dx = abs(dict_puzzle[i][0] - goal_state_dict[i][0])
+        dw = abs(dict_puzzle[i][1] - goal_state_dict[i][1])
+        d += (dx + dw)
+    return d
+
+def find_uhat(frontier, count, u):
+    if type(log(expansion_count, 2)) is int:
+        new_frontier = []
+        for node in frontier:
+            delay =
+            util = u(delay, t_exp, w_t, w_f, g, node)
+            new_frontier.append([])
+        return heapq.heapify(new_frontier)
+    else:
+        return frontier
+
+
 #test cases
 start_state = (0, 1, 2, 3, 5, 6, 7, 4, 9, 10, 11, 8, 13, 14, 15, 12)
 new_puz = Puzzle(start_state)
 print bugsy(new_puz, u)
+
+
+
+
+
+
